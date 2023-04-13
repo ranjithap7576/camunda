403d1f1751f1f68dd1a710233e389840315f177a
ranjith-keyvalue <ranjith.u@keyvalue.systems>
2023-04-13 11:42:52 +0530

    Embedded Camunda engine

---
+++ camunda-engine/src/main/java/com/auberon/cafm/CamundaEngineApplication.java
@@ -0,0 +1,13 @@
+package com.auberon.cafm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaEngineApplication.class, args);
+	}
+
+}
---
+++ camunda-engine/src/test/java/com/auberon/cafm/CamundaEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auberon.cafm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
